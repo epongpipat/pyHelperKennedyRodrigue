f8df088fb389bc0dba925566dce4a53b734db05b
epongpipat <epongpiat@gmail.com>
2021-11-02 22:18:41 -0500

    update init

--- pyHelperKennedyRodrigue/__init__.py
+++ pyHelperKennedyRodrigue/__init__.py
@@ -1,3 +1,6 @@
 import pkg_resources
 
 __version__ = pkg_resources.require("pyHelperKennedyRodrigue")[0].version
+
+from .get_airc_id_from_sub_ses import get_airc_id_from_sub_ses
+from .get_n_vols_from_task import get_n_vols_from_task
