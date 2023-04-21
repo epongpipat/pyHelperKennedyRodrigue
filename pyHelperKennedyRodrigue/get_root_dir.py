b0a7b3987790703ea9b08d6f1c187767e581a63b
Ekarin Pongpipat <epongpipat@gmail.com>
2023-04-21 15:18:31 -0500

    update get_root_dir for the new servers

--- pyHelperKennedyRodrigue/get_root_dir.py
+++ pyHelperKennedyRodrigue/get_root_dir.py
@@ -1,11 +1,37 @@
 import sys
+import pandas as pd
+import os
+import socket
+import pkg_resources
 
-def get_root_dir():
-    if sys.platform == 'darwin':
-        root_dir = '/Volumes'
-    elif sys.platform == 'linux':
-        root_dir = '/raid/data'
-    else:
-        raise Exception("could not determine root_dir")
-        return
+
+def get_root_dir(dir):
+    """
+    Get the root directory for a given directory.
+
+    Parameters
+    ----------
+    dir : str
+        The directory for which the root directory is desired. can be one of:
+        "kmk", "kmr", or "kenrod"
+
+    Returns
+    -------
+    root_dir : str
+        The root directory for the given directory.
+    """
+    dir_list = ["kmk", "kmr", "kenrod"]
+    if dir not in dir_list:
+        raise Exception("dir must be one of: {}".format(dir_list))
+
+    in_path = pkg_resources.resource_filename(__name__, "root_dir.csv")
+    df = pd.read_csv(in_path)
+    os = sys.platform
+    os = os.title()
+    hostname = socket.gethostname()
+    df_sub = df[df["dir"] == dir]
+    df_sub = df_sub[df_sub["os"] == os]
+    if os == "linux":
+        df_sub = df_sub[df_sub["hostname"] == hostname]
+    root_dir = df_sub["path"].values[0]
     return root_dir
