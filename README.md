277bc721f0be02ccd91f3df99a3621bc58ecfc75
Ekarin Pongpipat <epongpipat@gmail.com>
2023-04-21 15:19:34 -0500

    update readme and version

--- README.md
+++ README.md
@@ -6,7 +6,3 @@ Useful functions for working with Dr. Kennedy's and Dr. Rodrigue's data
 ```
 pip install git+https://github.com/epongpipat/pyHelperKennedyRodrigue.git
 ```
-
-## Functions
-- get_airc_id_from_sub_ses()
-- get_n_vols_from_task()
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-setup(name='pyHelperKennedyRodrigue',
-      version='0.1.0',
-      description='python functions to help access Dr. Kennedy\'s and Dr. Rodrigue\'s data',
-      url='',
-      author='Ekarin E. Pongpipat',
-      author_email='epongpipat@gmail.com',
-      license='MIT',
-      packages=['pyHelperKennedyRodrigue'],
-      install_requires=['numpy', 'pandas'],
-      zip_safe=False)
+setup(
+    name="pyHelperKennedyRodrigue",
+    version="0.2.0",
+    description="python functions to help access Dr. Kennedy's and Dr. Rodrigue's data",
+    url="",
+    author="Ekarin E. Pongpipat",
+    author_email="epongpipat@gmail.com",
+    license="MIT",
+    packages=["pyHelperKennedyRodrigue"],
+    install_requires=["numpy", "pandas"],
+    zip_safe=False,
+)
