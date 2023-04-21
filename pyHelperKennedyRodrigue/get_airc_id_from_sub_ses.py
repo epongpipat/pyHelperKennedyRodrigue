67b7157b209a70ebab4594b78537eb925497f752
epongpipat <epongpipat@gmail.com>
2023-04-21 16:40:47 -0500

    import test #4

--- pyHelperKennedyRodrigue/get_airc_id_from_sub_ses.py
+++ pyHelperKennedyRodrigue/get_airc_id_from_sub_ses.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from pyHelperKennedyRodrigue import get_root_dir
+from pyHelperKennedyRodrigue.get_root_dir import get_root_dir
 
 
 def get_airc_id_from_sub_ses(sub, ses):
